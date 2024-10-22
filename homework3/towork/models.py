ae2e670d22f0cd8c167274da9525b7fd6deaac31
luka gagnidze <163721597+lukanera@users.noreply.github.com>
2024-10-22 20:03:44 +0400

    Delete homework3 directory

--- homework3/towork/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-class towork(models.Model):
-    title = models.TextField(max_length=100)
-    description = models.TextField()
-    views = models.IntegerField(default=False)
-    is_published = models.BooleanField(default=0)
-    create_date = models.DateTimeField()
--- homework3/towork/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from .views import home, about, about_stalin
-
-urlpatterns = [
-    path('home/', home),
-    path('about/', about),
-    path('about_stalin/', about_stalin),
-]
